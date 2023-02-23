ea46a22e443e5831019bc25a0af94cbb4d492366
anhnhTEKY <anhnh@teky.vn>
2023-02-23 08:56:16 +0700

    yes

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 """Settings module"""
 
-FORFEIT = False
-FORFEIT_TIME = 600  # Time in seconds
-LEAGUE_CLIENT_PATH = None # Replace None with your path. Ex: 'C:\\Riot Games\\League of Legends'
+FORFEIT = True
+FORFEIT_TIME = 10*60  # Time in seconds
+LEAGUE_CLIENT_PATH = r'C:\\Riot Games\\League of Legends' # Replace None with your path. Ex: 'C:\\Riot Games\\League of Legends'
 TESSERACT_PATH = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
